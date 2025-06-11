97cc52309e3298abf22559e81c6ef688b66eeacb
m.lewes@egyptianbanks.net <minaanwer@Minas-MacBook-Pro.local>
2025-06-11 14:18:22 +0300

    init project

---
+++ backend/backendServices/transaction-api/src/test/java/com/egyptianbanks/ipn/transactionapi/TransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egyptianbanks.ipn.transactionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
